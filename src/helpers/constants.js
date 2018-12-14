0e3c97d2c2aa3058349d33931c0cf58a42b0d0c7
Leo Generali <leogen17@gmail.com>
2018-12-13 19:14:28 -0500

    Add more options to dropdown

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -6,12 +6,26 @@ export const MILE = 1609.34 * METER;
 const QUARTERMILE = 400 * METER;
 const HALFMILE = 800 * METER;
 const FIFTEEN = 1500 * METER;
+const THREEK = 3 * KILOMETER;
 const FIVEK = 5 * KILOMETER;
 const TENK = 10 * KILOMETER;
+const FIFTEENK = 15 * KILOMETER;
+const TENMILE = 16.0934 * KILOMETER;
+const TWENTYK = 20 * KILOMETER;
+const HALFMARATHON = 21.0975 * KILOMETER;
+const MARATHON = 42.195 * KILOMETER;
 
 export const DISTANCE_OPTIONS = [
   { label: '400m', value: QUARTERMILE },
   { label: '800m', value: HALFMILE },
   { label: '1500m', value: FIFTEEN },
-  { label: 'Mile', value: MILE }
+  { label: 'Mile', value: MILE },
+  { label: '3k/Steeplechase', value: THREEK },
+  { label: '5k', value: FIVEK },
+  { label: '10k', value: TENK },
+  { label: '15k', value: FIFTEENK },
+  { label: '10 mile', value: TENMILE },
+  { label: '20k', value: TWENTYK },
+  { label: 'Half Marathon', value: HALFMARATHON },
+  { label: 'Marathon', value: MARATHON }
 ];
